69e66365813f84c12659737131f76642848ae55f
Maria Heloísa <heloisamartins06@hotmail.com>
2024-09-03 10:46:25 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- cartorio_heloisa1.c
+++ cartorio_heloisa1.c
@@ -128,7 +128,8 @@ int main()//função principal
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1- Resgitrar nomes\n");
 		printf("\t2- Consultar os nomes\n");
-		printf("\t3- Deletar nomes\n\n");
+		printf("\t3- Deletar nomes\n");
+		printf("\t4- Sair do sistema\n\n");
 		printf("opcao: ");//fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -148,6 +149,11 @@ int main()//função principal
 			
 			case 3:
 			deletar(); //chamada de funções
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
